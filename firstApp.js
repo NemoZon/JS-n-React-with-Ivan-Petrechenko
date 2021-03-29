a04ad590031a91ff5621518b276051f226ec73db
Gleb Bushukin <gleb.bushukin@gmail.com>
2021-03-30 00:09:23 +0200

    lesson 24

--- firstApp.js
+++ firstApp.js
@@ -1,39 +1,33 @@
-'use strict';
 /* Задание на урок:
-1) Первую часть задания повторить по уроку
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных genres
-P.S. Функции вызывать не обязательно */
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
-let numberOfFilms,
-    favoriteGenre;
-
-start();
+'use strict';
 
 const personalMovieDB = {
-    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
 
-rememberMyFilms();
-detectPersonalLevel();
-writeYourGenres();
-showMyDB();
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+personalMovieDB.start = function () {
+    let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    personalMovieDB.count = numberOfFilms;
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(numberOfFilms)) {
         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        personalMovieDB.count = numberOfFilms;
     }
-}
+};
 
-function detectPersonalLevel(){
+personalMovieDB.detectPersonalLevel = function (){
     if(personalMovieDB.count<10){
         console.log("Просмотрено довольно мало фильмов");
     }else if(10<= personalMovieDB.count && personalMovieDB.count<=30){
@@ -43,9 +37,9 @@ function detectPersonalLevel(){
     }else{
         console.log("Произошла ошибка");
     }
-}
+};
 
-function rememberMyFilms(){
+personalMovieDB.rememberMyFilms = function (){
     for ( let i = 0; i<2; i++){
         let a = prompt('Один из последних просмотренных фильмов?',""),
             b = prompt('На сколько оцените его?',"");
@@ -58,17 +52,39 @@ function rememberMyFilms(){
             personalMovieDB.movies[a]=b;
         }
     }
-}
+};
+personalMovieDB.toggleVisibleMyDB = function (){
+    if (personalMovieDB.privat==false){
+        personalMovieDB.privat=true;
+    } else{
+        personalMovieDB.privat = false;
+    }
+};
 
-function showMyDB() {
+personalMovieDB.showMyDB = function () {
     if (personalMovieDB.privat==false){
         console.log(personalMovieDB);
     }
-}
+};
 
-function writeYourGenres(){
+personalMovieDB.writeYourGenres = function (){
+    let favoriteGenre;
     for (let i=1;i<4;i++){
-        favoriteGenre=prompt(`Ваш любимый жанр под номером ${i}`);
+        favoriteGenre = prompt(`Ваш любимый жанр под номером ${i}`);
         personalMovieDB.genres[i-1]=favoriteGenre;
+        if (favoriteGenre == '' || favoriteGenre == null) {
+            i--;
+        }
     }
-}
+    personalMovieDB.genres.forEach(function(item, i){
+        console.log(`Любимый жанр ${i+1} - это ${item}`);
+    });
+    
+};
+
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.writeYourGenres();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB();
